5043d44cf352a8fd1c41cadffc7950a9a1f861a1
shi-n <shi-n_xyz@cloudsquare.jp>
2019-09-28 21:23:17 +0900

    Initial Commit

---
+++ BeautifulWord/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeautifulWord
+//
+//  Created by Shin Inaba on 2019/09/28.
+//  Copyright © 2019 shi-n. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
